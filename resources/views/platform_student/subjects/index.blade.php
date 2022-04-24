2ad60e7b2c9cf31937702951d62e7fb51c5ec82e
Ben Ad <benadida05@gmail.com>
2022-04-24 04:36:04 +0200

    feat(subjects)

--- resources/views/platform_student/subjects/index.blade.php
+++ resources/views/platform_student/subjects/index.blade.php
@@ -3,22 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <h1>Index</h1>
-
-        <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">Nom</th>
-              </tr>
-            </thead>
-            <tbody>
-                @foreach ($subjects as $subject)
-                    <tr onclick="window.location='{{ route('subjects.show', ['subject' => $subject->id]) }}';" style="cursor:pointer" class="tr-hover">
-                        <th scope="row">{{ $subject->name }}</th>
-                    </tr>
-                @endforeach
-            </tbody>
-          </table>
+        <h1>Mes matières</h1>
+        @foreach ($subjects as $subject)
+          <div class="card" style="width: 18rem;margin: 10px">
+            <img class="card-img-top" src="https://via.placeholder.com/150" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">{{ $subject->name }}</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="{{ route('subjects.show', ['subject' => $subject->id]) }}" class="btn btn-primary">Go</a>
+            </div>
+          </div>
+        @endforeach
     </div>
 </div>
 @endsection
